2c6b61bcdb9deeb9a25d05eacb12ee80a4d677b9
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-15 20:45:41 -0800

    Add logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
+  # ##################################
+  # AUTH
+  # ##################################
+  # Register user
   get "sign_up", to: "registration#new"
   post "sign_up", to: "registration#create"
+  # Login In User 
+  get "sign_in", to: "session#new"
+  post "sign_in", to: "session#create"
+  # Logout User
+  delete "logout", to: "session#destroy"
+
+
+
 
   root to: "main#index"
 end
